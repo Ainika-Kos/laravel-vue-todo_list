ed2deef733115e93ecaf7ecb1324a6dc508d1562
Ainika <aina.kostina@gmail.com>
2021-04-11 13:58:49 +0300

    Added responsive style

--- resources/js/vue/taskView/taskView.vue
+++ resources/js/vue/taskView/taskView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="taskView">
     <div class="taskView_input">
-      <input
-        type="checkbox"
-        @change="triggerTask"
-        v-model="item.completed"
-        :id="item.name"
-      />
-      <span class="checkmark"></span>
       <label
         :class="[item.completed ? 'completed' : '', 'taskText']"
         :for="item.name"
       >
         {{ item.name }}
+        <input
+          type="checkbox"
+          @change="triggerTask"
+          v-model="item.completed"
+          :id="item.name"
+        />
+        <span class="checkmark"></span>
       </label>
     </div>
     <div>
